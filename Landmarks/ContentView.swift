8b8d09e13024217501e865957c845b9c3f59b6bd
dabutvin <dan.butvinik@gmail.com>
2025-04-19 20:34:55 -0400

    put it all together

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -9,18 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("Turtle Rock")
-                .font(.title)
-            HStack {
-                Text("Joshua Tree National Park")
-                    .font(.subheadline)
-                Spacer()
-                Text("California")
-                    .font(.subheadline)
+        VStack {
+            MapView()
+                .frame(height: 300)
+
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundStyle(.secondary)
+                Divider()
+
+                Text(
+                    "A natural rock formation in Joshua Tree National Park, California."
+                )
+                .font(.title2)
+
+                Text(
+                    "This park is a popular destination for hikers and photographers, known for its unique rock formations and desert landscapes."
+                )
             }
+            .padding()
+            Spacer()
         }
-        .padding()
     }
 }
 
