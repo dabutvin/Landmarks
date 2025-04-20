324ac5e6d702c2793ebeb302cb6a9b4a90dc9454
dabutvin <dan.butvinik@gmail.com>
2025-04-19 20:52:30 -0400

    add landmarks dataset

---
+++ Landmarks/Model/Landmark.swift
@@ -0,0 +1,55 @@
+//
+//  Landmark.swift
+//  Landmarks
+//
+//  Created by Dan on 4/19/25.
+//
+
+import CoreLocation
+import Foundation
+import SwiftUI
+
+struct Landmark: Hashable, Codable {
+    var id: Int
+    var name: String
+    var park: String
+    var state: String
+    var description: String
+
+    private var imageName: String
+    var image: Image {
+        Image(imageName)
+    }
+
+    private var coordinates: Coordinates
+    var locationCoordinates: CLLocationCoordinate2D {
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+    }
+    struct Coordinates: Hashable, Codable {
+        var longitude: Double
+        var latitude: Double
+    }
+}
+
+/*
+ Landmark
+ {
+ "name": "Turtle Rock",
+ "category": "Rivers",
+ "city": "Twentynine Palms",
+ "state": "California",
+ "id": 1001,
+ "isFeatured": true,
+ "isFavorite": true,
+ "park": "Joshua Tree National Park",
+ "coordinates": {
+ "longitude": -116.166868,
+ "latitude": 34.011286
+ },
+ "description": "Suscipit inceptos est felis purus aenean aliquet adipiscing diam venenatis, augue nibh duis neque aliquam tellus condimentum sagittis vivamus, cras ante etiam sit conubia elit tempus accumsan libero, mattis per erat habitasse cubilia ligula penatibus curae. Sagittis lorem augue arcu blandit libero molestie non ullamcorper, finibus imperdiet iaculis ad quam per luctus neque, ligula curae mauris parturient diam auctor eleifend laoreet ridiculus, hendrerit adipiscing sociosqu pretium nec velit aliquam. Inceptos egestas maecenas imperdiet eget id donec nisl curae congue, massa tortor vivamus ridiculus integer porta ultrices venenatis aliquet, curabitur et posuere blandit magnis dictum auctor lacinia, eleifend dolor in ornare vulputate ipsum morbi felis. Faucibus cursus malesuada orci ultrices diam nisl taciti torquent, tempor eros suspendisse euismod condimentum dis velit mi tristique, a quis etiam dignissim dictum porttitor lobortis ad fermentum, sapien consectetur dui dolor purus elit pharetra. Interdum mattis sapien ac orci vestibulum vulputate laoreet proin hac, maecenas mollis ridiculus morbi praesent cubilia vitae ligula vel, sem semper volutpat curae mauris justo nisl luctus, non eros primis ultrices nascetur erat varius integer.",
+ "imageName": "turtlerock"
+ }
+ */
