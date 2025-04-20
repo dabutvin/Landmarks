9f49166b3d8e17239c1157855abcef2fdb35a737
dabutvin <dan.butvinik@gmail.com>
2025-04-19 21:35:56 -0400

    set up list navigation

--- Landmarks/Views/ContentView.swift
+++ Landmarks/Views/ContentView.swift
@@ -9,38 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height: 300)
-
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
-
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
-                HStack {
-                    Text("Joshua Tree National Park")
-                    Spacer()
-                    Text("California")
-                }
-                .font(.subheadline)
-                .foregroundStyle(.secondary)
-                Divider()
-
-                Text(
-                    "A natural rock formation in Joshua Tree National Park, California."
-                )
-                .font(.title2)
-
-                Text(
-                    "This park is a popular destination for hikers and photographers, known for its unique rock formations and desert landscapes."
-                )
-            }
-            .padding()
-            Spacer()
-        }
+        LandmarkList()
     }
 }
 
